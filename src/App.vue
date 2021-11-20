e919cdfb9aa0662fda466bf8bdd756a4b93459a7
Zane Thomas <zane.thomas@gmail.com>
2021-11-20 14:51:33 -0700

    it's a start

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import FirstTest from './components/FirstTest.vue'
+</script>
+
+<template>
+  <FirstTest />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
